747ba8deee2edfa91e0c3547ada14ea0508f14f0
spencernikolaeff <59879326+spencernikolaeff@users.noreply.github.com>
2022-04-23 15:15:00 -0600

    fix controller
    changed controller so that no logic is involved in the controller

--- NumberGuessGame/src/main/java/NumberGame/controller/NumberGuessGameController.java
+++ NumberGuessGame/src/main/java/NumberGame/controller/NumberGuessGameController.java
@@ -5,7 +5,9 @@ import NumberGame.models.GameData;
 import NumberGame.models.RoundData;
 import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,13 +20,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class NumberGuessGameController {
     // Create DAO object
     private final NumberGuessGameDao dao;
-    private List<GameData> AllGames;
-    private int currGameIndex;
     
     // Constructor for Dependency Injection
     public NumberGuessGameController(NumberGuessGameDao dao){
         this.dao = dao;
-        currGameIndex = 0;
     }
     
     //testing
@@ -49,15 +48,12 @@ public class NumberGuessGameController {
     @PostMapping("/begin")
     public String BeginGame() {
         
-        //add a new game to the games list
-        AllGames.add(dao.createNewGame());
+        //create integer game id value
+        int retId = dao.createNewGame().getGameId();
         
         
         //string output for successful creation of a new game, combined with the most recently created games ID
-        String ret = "Application Status 201: new game successfully created with id: " + Integer.toString(AllGames.get(currGameIndex).getGameId());
-        
-        //increment current game index value
-        currGameIndex++;
+        String ret = "Application Status 201: new game successfully created with id: " + retId;
         
         return ret;
     }
@@ -77,17 +73,11 @@ public class NumberGuessGameController {
     //we return the newly created round object
     
     @PostMapping("/guess")
-    public RoundData guess(int value) { //without passing gameid since we know which game we're currently updating
+    public RoundData guess(@PathVariable int id, @RequestBody int guess) { //without passing gameid since we know which game we're currently updating
         
-        //create new round object using user guess
-        //need to fix Dao to take in a user guess and make a new round object with a
-        //properly formatted time object as well
-        RoundData newRound = dao.addNewRoundData(value);
+        //return the newly created round object
+        return dao.addNewRoundData(id,guess);
         
-        //add new round to current game object in list
-        AllGames.get(currGameIndex).addRound(newRound);
-        
-        return newRound;
     }
     
     
@@ -96,14 +86,7 @@ public class NumberGuessGameController {
     //Returns a list of all games. Be sure in-progress games do not display their answer
     @GetMapping("/game")
     public List<GameData> viewAllGames(){
-        
-        //create new list so we can alter the in-progress games answer
-        List<GameData> ret = AllGames;
-        
-        //set to a custom answer id to avoid showing the answer of the in-progress game
-        ret.get(currGameIndex).setAnswerId(99999);
-        
-        return ret;
+        return dao.getAllGames();
     }
     
     
@@ -111,18 +94,8 @@ public class NumberGuessGameController {
     //GAME/GAMEID (GET METHOD)
     //Returns a specific game based on ID. Be sure in-progress games do not display their answer
     @GetMapping("/game/gameid")
-    public GameData viewGameById(int id) {
-        
-        //find game by id
-        GameData ret = dao.getGameById(id);
-        
-        //if that game hasn't been won we change the answer id to a custom hidden id
-        if(!ret.getGameWon()) {
-            ret.setAnswerId(99999);
-        }
-        
-        //return specific game object
-        return ret;
+    public GameData viewGameById(@RequestBody int id) {
+        return dao.getGameById(id);
     }
     
     
@@ -130,13 +103,8 @@ public class NumberGuessGameController {
     //ROUNDS/GAMEID (GET METHOD)
     //Returns a list of rounds for the specified game sorted by time
     @GetMapping("/rounds/gameid")
-    public List<RoundData> viewGameRounds(int id) {
-        
-        //create temp game object by specified id
-        GameData temp = dao.getGameById(id);
-        
-        //return list of rounds for that game object
-        return temp.getRounds();
+    public List<RoundData> viewGameRounds(@RequestBody int id) {
+        return dao.getGameRoundsById(id);
     }
     
 }
