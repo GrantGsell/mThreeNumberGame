5d31f3b2fcd53f3a2612fdd770e5a8505b3a8af7
GrantGsell <47372698+GrantGsell@users.noreply.github.com>
2022-04-23 14:21:22 -0400

    Added methods in DAO interface

--- NumberGuessGame/src/main/java/NumberGame/data/NumberGuessGameDao.java
+++ NumberGuessGame/src/main/java/NumberGame/data/NumberGuessGameDao.java
@@ -1,13 +1,88 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
 package NumberGame.data;
+
+import NumberGame.models.GameData;
+import NumberGame.models.RoundData;
+import java.util.List;
 
 /**
  *
  * @author Grant
  */
 public interface NumberGuessGameDao {
+    /**
+     * Creates a new game with a new four digit number for guessing.
+     * 
+     * The new game id is created by obtaining the last game id from the Games
+     *   table.
+     * 
+     * Generates a new randomized id for a possible answer. From 1000-9999 there
+     *   are 4536 numbers who digits are unique. So for four digit numbers the
+     *   random number to be generated is in the range [0, 4535]. The id will
+     *   then be used to select a corresponding four digit number from the 
+     *   All possible answers table
+     * 
+     * @return 
+     */
+    GameData createNewGame();
+    
+    
+    /**
+     * Obtains the id for the last played game. If this is the first game the 
+     *   method will return null. Otherwise it will return the integer id of the
+     *   last game played.
+     * 
+     * @return 
+     */
+    int getLastGameId();
+    
+    
+    /**
+     * Obtains a list of all Game objects, which contains Game Ids, and if the 
+     *   game is finished (won) or not.
+     * 
+     * @return A list of Game objects
+     */
+    List<GameData> getAllGames();
+    
+    
+    /**
+     * Adds round data to the RoundData table. First obtains the id of the last
+     *   round to be played and increments it by 1 to create a roundId for this
+     *   new round. Data is then placed into the RoundData table, after which 
+     *   the new roundId and provided gameId data are placed in the rounds
+     *   bridge table.
+     * 
+     * @param gameId the id for the associated game.
+     */
+    void addNewRoundData(int gameId);
+    
+    /**
+     * Obtains the id for the last round played. If this is the first round the 
+     *   method will return null. Otherwise it will return the integer id of the
+     *   last round played.
+     * 
+     * @return 
+     */
+    int getLastRoundId();
+    
+    /**
+     * Obtains a game object based on its game id.
+     * 
+     * @param gameId the associated id for the GameData object.
+     * @return the GameData object associated with gameId if it exists
+   otherwise null.
+     */
+    GameData getGameById(int gameId);
+    
+    /**
+     * Obtains a list of all round data objects associated with the given 
+     *   gameId.
+     * 
+     * @param gameId the id of the GameData object whose rounds we want 
+   to see.
+     * @return a list of RoundData objects containing the data associated with
+     *   each round of the game specified by gameId.
+     */
+    List<RoundData> getAllRoundsOneGame(int gameId);
     
 }
--- NumberGuessGame/src/main/java/NumberGame/data/NumberGuessGameDaoImpl.java
+++ NumberGuessGame/src/main/java/NumberGame/data/NumberGuessGameDaoImpl.java
@@ -1,13 +1,91 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package NumberGame.data;
+
+import NumberGame.models.GameData;
+import NumberGame.models.RoundData;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
 
 /**
  *
  * @author Grant
  */
-public class NumberGuessGameDaoImpl {
+@Repository
+public class NumberGuessGameDaoImpl implements NumberGuessGameDao{
+    // JDBC template variable
+    private final JdbcTemplate jdbcTemplate;
+
+    // Constructor for Spring Boot REST Service with JDBC Template
+    @Autowired
+    NumberGuessGameDaoImpl(JdbcTemplate jdbcTemplate){
+        this.jdbcTemplate = jdbcTemplate;
+    }
     
+    
+    @Override
+    public GameData createNewGame() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public int getLastGameId() {
+        // Create statement string
+        final String sql = "SELECT * FROM Games ORDER BY gameId DESC LIMIT 1";
+        
+        // Obtain GameData object for the last game played,(single element list)
+        List<GameData> lastGameList = jdbcTemplate.query(sql, new GameDataMapper());
+        
+        // Get the last GameData object
+        GameData lastGame = lastGameList.get(0);
+        
+        // Return the GameId for the last game object
+        return lastGame.getGameId();
+    }
+
+    @Override
+    public List<GameData> getAllGames() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void addNewRoundData(int gameId) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public int getLastRoundId() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public GameData getGameById(int gameId) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public List<RoundData> getAllRoundsOneGame(int gameId) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+    
+    /**
+     * Creates mapping class to map MySQL data from table into NumberGuessGame 
+     *   object
+     */
+    private static final class GameDataMapper implements RowMapper<GameData>{
+
+        @Override
+        public GameData mapRow(ResultSet rs, int i) throws SQLException {
+            GameData game = new GameData();
+            game.setGameId(rs.getInt("gameId"));
+            game.setAnswerId(rs.getInt("answerId"));
+            game.setGameWon(rs.getBoolean("gameWon"));
+            return game;
+        }
+        
+    }
 }
