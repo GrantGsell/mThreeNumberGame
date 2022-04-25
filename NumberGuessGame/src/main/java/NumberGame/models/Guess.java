83f699350725d84990d2a2e7eaec907244e645e5
spencernikolaeff <59879326+spencernikolaeff@users.noreply.github.com>
2022-04-25 08:15:20 -0600

    added guess object in models

---
+++ NumberGuessGame/src/main/java/NumberGame/models/Guess.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package NumberGame.models;
+
+/**
+ *
+ * @author 17202
+ */
+public class Guess {
+    private int id;
+    private int guess;
+    
+    public int getId() {
+        return this.id;
+    }
+    
+    public int getGuess() {
+        return this.guess;
+    }
+}
